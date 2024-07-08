81b9734f947d6358f1b61851ba1b6c52895494ce
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-07-08 16:10:46 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+
+        StringTokenizer st = null;
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
